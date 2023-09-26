31eff2635c2f28610c044fc67e55b1722278ea4b
Adnan Junaid <adnanjunaid76279@gmail.com>
2023-09-26 10:46:41 +0500

    forget password api set

---
+++ resources/views/emails/forget_password.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Forget Passward</title>
+</head>
+<body>
+    <h3>Hi {{$details['Name']}},</h3>
+    <p>Your password change successfully!</p><br>
+    <p>Your new password is <b>{{$details['Password']}}</b></p><br>
+    <p>Thank you for visiting <a href="https://pricemanager.pk">Price Manager</a></p>
+</body>
+</html>
